785934c053a6b7beef3765d2073c3ad26cb447db
gurleen124 <gurleen.kaur@lpinfotech.com>
2024-05-07 11:31:25 +0530

    faq created

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
-let minus= document.getElementById("minus");
-let plus = document.getElementById("plus");
-let answer = document.getElementById("answer2");
-document.getElementById("plus").addEventListener("click", function(){
-    showAnswer();
-})
+let items = document.querySelectorAll(".accordion");
 
-function showAnswer(){
-    minus.classList.remove("hidden");
-    answer.classList.remove("hidden");
-    plus.classList.add("hidden");
-}
+items.forEach( item => {
+    item.addEventListener('click', function(){
+        const answers = item.querySelector(".answer");
+        answers.classList.toggle("hidden");
+
+        const questions = item.querySelector(".question");
+        const textColor = questions.querySelector("p");
+        textColor.classList.toggle("text-gray-600")
+        
+        const plus = item.querySelector(".plus");
+        const minus = item.querySelector(".minus");
+        plus.classList.toggle("hidden");
+        minus.classList.toggle("hidden");
+
+    })
+});
